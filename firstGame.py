1177ccb4eb37c445e3606a805c8fd9a7526ccb2e
Riccardo Foti <riccardofoti97@gmail.com>
2020-05-29 15:56:57 +0100

    Lesson number 2

--- firstGame.py
+++ firstGame.py
@@ -6,29 +6,46 @@ win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("First Game")
 
 x = 50
-y = 50
+y = 425
 width = 40
 height = 60
 vel = 5
 
+isJump = False
+jumpCount = 10
+
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
     keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_LEFT]:
+
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < (500 - width - vel):
         x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
+
+    if not(isJump): # Checks if user is not jumping
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < (500 - height - vel):
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            jumpCount -= 1
+            print("y : " + str(y) + "    jumpCount : "+ str(jumpCount)) # Print "y" and "jumpCount" values in realtime
+        else: # This will execute if our jump is finished
+            jumpCount = 10
+            isJump = False
+            # Reset Variables
+        
     win.fill((0, 0, 0))
     pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
